bf17c9a8d82b2efa92962143c34912ebe1625b01
bcollins@funtech.com <bcollins@funtech.com>
2016-09-14 11:12:56 -0400

    Day 1/2

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SwApiExamplesCs
 Some random SOLIDWORKS API usage examples from modern C#.
+
+The examples **ARE NOT** the API Fundamentals examples. For that, it is highly recommend that you take an official SOLIDWORKS API Fundamentals course (check w/your VAR to see if they offer it).
+
+These examples **ARE** just some random bits and pieces that have been conjured up while exploring the API object model with the added goal in mind of using modern C# (4 and beyond) code.
+
+## To run the examples...
+
+- Download/install the training files for the API Fundamentals class from SOLIDWORKS. The code examples don't matter and this project will just make use of the CAD files for the examples.
+- To avoid unnecessary grief, be sure to install the training files to the default location because some examples simply hard-code paths to certain traning folders and/or files for simplicity when an example needs to open a certain file or save an output, etc.
---
+++ SwConsole/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SolidWorks.Interop.sldworks;
+using System.IO;
+using SolidWorks.Interop.swconst;
+using System.Diagnostics;
+
+namespace SwConsole
+{
+    class Program
+    {
+        const string EXAMPLESDIR = @"C:\SolidWorks Training Files\API Fundamentals\";
+
+        static SldWorks swApp;
+
+        /// <summary>
+        /// Attempts to open the argument file in SOLIDWORKS.
+        /// </summary>
+        /// <param name="filePath">The full file path</param>
+        /// <returns>The <see cref="ModelDoc2"/> or null if open fails for any reason.</returns>
+        static ModelDoc2 OpenDoc(string filePath)
+        {
+            // init return value
+            ModelDoc2 swModel = null;
+
+            Debug.WriteLine("Opening: " + filePath);
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File does not exist...");
+            }
+            else
+            {
+                // TODO: Really open...
+                var swDocSpec = (DocumentSpecification)swApp.GetOpenDocSpec(filePath);
+
+                swDocSpec.Silent = true;
+
+                swModel = swApp.OpenDoc7(swDocSpec);
+
+
+                // TODO: Handle open doc errors and warnings.
+
+                if (swDocSpec.Error != 0)
+                {
+                    // TODO: Handle errors
+                }
+
+                //if (swDocSpec.Warning != 0)
+                //{
+                //    // TODO: Handle warnings
+                //}
+
+                // Maybe a more preferable way than above (depends on the situation probably)...
+
+                switch ((swFileLoadWarning_e)swDocSpec.Warning)
+                {
+                    case swFileLoadWarning_e.swFileLoadWarning_IdMismatch:
+                        break;
+                    case swFileLoadWarning_e.swFileLoadWarning_ReadOnly:
+                        break;
+                    case swFileLoadWarning_e.swFileLoadWarning_SharingViolation:
+                        break;
+                    case swFileLoadWarning_e.swFileLoadWarning_DrawingANSIUpdate:
+                        break;
+                    case swFileLoadWarning_e.swFileLoadWarning_SheetScaleUpdate:
+                        break;
+                    case swFileLoadWarning_e.swFileLoadWarning_NeedsRegen:
+                        break;
+                    case swFileLoadWarning_e.swFileLoadWarning_BasePartNotLoaded:
+                        break;
+                    case swFileLoadWarning_e.swFileLoadWarning_AlreadyOpen:
+                        break;
+                    case swFileLoadWarning_e.swFileLoadWarning_DrawingsOnlyRapidDraft:
+                        break;
+                    case swFileLoadWarning_e.swFileLoadWarning_ViewOnlyRestrictions:
+                        break;
+                    case swFileLoadWarning_e.swFileLoadWarning_ViewMissingReferencedConfig:
+                        break;
+                    case swFileLoadWarning_e.swFileLoadWarning_DrawingSFSymbolConvert:
+                        break;
+                    case swFileLoadWarning_e.swFileLoadWarning_RevolveDimTolerance:
+                        break;
+                    case swFileLoadWarning_e.swFileLoadWarning_ModelOutOfDate:
+                        break;
+                    case swFileLoadWarning_e.swFileLoadWarning_DimensionsReferencedIncorrectlyToModels:
+                        break;
+                    case swFileLoadWarning_e.swFileLoadWarning_ComponentMissingReferencedConfig:
+                        break;
+                    case swFileLoadWarning_e.swFileLoadWarning_InvisibleDoc_LinkedDesignTableUpdateFail:
+                        break;
+                    case swFileLoadWarning_e.swFileLoadWarning_MissingDesignTable:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return swModel;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Starting SOLIDWORKS...");
+
+            swApp = new SldWorks();
+
+            swApp.Visible = true;
+
+            Console.WriteLine("SOLIDWORKS current language: " + swApp.GetCurrentLanguage());
+
+            Console.WriteLine("Open a document...");
+
+            OpenDoc(EXAMPLESDIR + @"Lesson02 - Object Model Basics\Case Study\sheetmetalsample.SLDASM");
+
+            Console.WriteLine("Closing document...");
+
+
+            Console.WriteLine("\nPress any key to exit");
+
+            Console.ReadKey();
+        }
+    }
+}
